3511888047140b2e467b7d9d318c040d56950dd2
recamanto <cedealejandro27@gmail.com>
2026-03-13 21:10:40 -0400

    creacion_de_plantillas

--- app.js
+++ app.js
@@ -8,13 +8,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.port || 3000;
 
+
+
+
 app.use(express.static(path.join(__dirname + "/public")));
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {titulo : "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"});
+});
+
+app.get("/service", (req, res) => {
+    res.render("service", {titulo : "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"});
 });
 
 app.get("/:page", (req, res) => {
